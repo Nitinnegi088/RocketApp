1e4fe217ae4de9e9f14c3c5e6754a02049a748a8
NitinNegiEE <nitin.n@theextraaedge.com>
2022-12-12 19:07:00 +0530

    Update MainActivity.kt
    s1

--- app/src/main/java/com/example/racketapp/UI/MainActivity.kt
+++ app/src/main/java/com/example/racketapp/UI/MainActivity.kt
@@ -33,8 +33,6 @@ class MainActivity : AppCompatActivity(),RocketAdapter.OnItemClickListener{
         recyclerView = findViewById(R.id.rv_rocket)
         tv_error = findViewById(R.id.text_view_error)
         tv_loading = findViewById(R.id.progress_bar)
-
-
         rocketAdapters = RocketAdapter(this,ArrayList<RocketDataItem>(),this)
         recyclerView.apply {
             setHasFixedSize(true)
